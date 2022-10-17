8da48f4453c39f1f3eb53fa2e5490e99a0f1cba5
JChenoweth <jchenoweth@Jennifers-MBP.local>
2022-10-16 19:35:31 -0500

    adds player entries and deletions and edits

--- controllers/player.js
+++ controllers/player.js
@@ -1,6 +1,6 @@
 // Import Dependencies
 const express = require('express')
-const Example = require('../models/example')
+const Player = require('../models/player')
 
 // Create router
 const router = express.Router()
@@ -23,12 +23,12 @@ router.use((req, res, next) => {
 
 // index ALL
 router.get('/', (req, res) => {
-	Example.find({})
-		.then(examples => {
+	Player.find({})
+		.then(players => {
 			const username = req.session.username
 			const loggedIn = req.session.loggedIn
 			
-			res.render('examples/index', { examples, username, loggedIn })
+			res.render('players/index', { players, username, loggedIn })
 		})
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
@@ -39,9 +39,9 @@ router.get('/', (req, res) => {
 router.get('/mine', (req, res) => {
     // destructure user info from req.session
     const { username, userId, loggedIn } = req.session
-	Example.find({ owner: userId })
-		.then(examples => {
-			res.render('examples/index', { examples, username, loggedIn })
+	Player.find({ owner: userId })
+		.then(players => {
+			res.render('players/index', { players, username, loggedIn })
 		})
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
@@ -51,7 +51,7 @@ router.get('/mine', (req, res) => {
 // new route -> GET route that renders our page with the form
 router.get('/new', (req, res) => {
 	const { username, userId, loggedIn } = req.session
-	res.render('examples/new', { username, loggedIn })
+	res.render('players/new', { username, loggedIn })
 })
 
 // create -> POST route that actually calls the db and makes a new document
@@ -59,10 +59,10 @@ router.post('/', (req, res) => {
 	req.body.ready = req.body.ready === 'on' ? true : false
 
 	req.body.owner = req.session.userId
-	Example.create(req.body)
-		.then(example => {
-			console.log('this was returned from create', example)
-			res.redirect('/examples')
+	Player.create(req.body)
+		.then(player => {
+			console.log('this was returned from create', player)
+			res.redirect('/players')
 		})
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
@@ -72,10 +72,10 @@ router.post('/', (req, res) => {
 // edit route -> GET that takes us to the edit form view
 router.get('/:id/edit', (req, res) => {
 	// we need to get the id
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
-			res.render('examples/edit', { example })
+	const playerId = req.params.id
+	Player.findById(playerId)
+		.then(player => {
+			res.render('players/edit', { player })
 		})
 		.catch((error) => {
 			res.redirect(`/error?error=${error}`)
@@ -84,12 +84,12 @@ router.get('/:id/edit', (req, res) => {
 
 // update route
 router.put('/:id', (req, res) => {
-	const exampleId = req.params.id
+	const playerId = req.params.id
 	req.body.ready = req.body.ready === 'on' ? true : false
 
-	Example.findByIdAndUpdate(exampleId, req.body, { new: true })
-		.then(example => {
-			res.redirect(`/examples/${example.id}`)
+	Player.findByIdAndUpdate(playerId, req.body, { new: true })
+		.then(player => {
+			res.redirect(`/players/${player.id}`)
 		})
 		.catch((error) => {
 			res.redirect(`/error?error=${error}`)
@@ -98,11 +98,11 @@ router.put('/:id', (req, res) => {
 
 // show route
 router.get('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
+	const playerId = req.params.id
+	Player.findById(playerId)
+		.then(player => {
             const {username, loggedIn, userId} = req.session
-			res.render('examples/show', { example, username, loggedIn, userId })
+			res.render('players/show', { player, username, loggedIn, userId })
 		})
 		.catch((error) => {
 			res.redirect(`/error?error=${error}`)
@@ -111,10 +111,10 @@ router.get('/:id', (req, res) => {
 
 // delete route
 router.delete('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findByIdAndRemove(exampleId)
-		.then(example => {
-			res.redirect('/examples')
+	const playerId = req.params.id
+	Player.findByIdAndRemove(playerId)
+		.then(player => {
+			res.redirect('/players')
 		})
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
